d005d59d5ae04f8cc04ed7c0e5d654c21ebc8c09
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-30 22:53:45 +0200

    ex4 solution

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,12 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+
+// TODO (04) Create an INSTANCE variable to store the VideosDatabase singleton.
+private lateinit var INSTANCE: VideosDatabase
 
 @Dao
 interface VideoDao {
@@ -33,14 +34,30 @@ interface VideoDao {
 }
 
 // TODO (01) Create an abstract VideosDatabase class that extends RoomDatabase.
-
 // TODO (02) Annotate VideosDatabase with @Database,including entities and version.
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    // TODO (03) Inside VideosDatabase, create abstract val videoDao.
+    abstract val videoDao: VideoDao
 
-// TODO (03) Inside VideosDatabase, create abstract val videoDao.
-
-// TODO (04) Create an INSTANCE variable to store the VideosDatabase singleton.
+}
 
 // TODO (05) Define a function getDatabase() that returns the VideosDatabase INSTANCE.
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        // TODO (06) Inside getDatabase(), before returning INSTANCE, use a synchronized{} block to
+        // check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
+}
 
-// TODO (06) Inside getDatabase(), before returning INSTANCE, use a synchronized{} block to
-// check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+
+
+
