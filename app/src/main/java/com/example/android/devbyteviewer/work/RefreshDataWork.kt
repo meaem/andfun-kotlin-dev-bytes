e7337a2cbdda2ea0441c3b2f87ebcf1128c0e48b
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-30 23:56:37 +0200

    ex7 solution

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -17,9 +17,32 @@
 
 package com.example.android.devbyteviewer.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
 // TODO (01) Create the RefreshDataWorker class, extend it from CoroutineWorker, and
 // pass in a Context and WorkerParams.
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
 
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+
+    }
+
+
+}
 // TODO (02) Override the required doWork() method, and create variables for the
 // database and the repository.
 
