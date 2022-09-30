49687e1709daf19b6c44978080b152610c1df0bd
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-30 16:08:10 +0200

    ex1 solution

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -26,8 +27,6 @@ import com.squareup.moshi.JsonClass
  * using them.
  */
 
-// TODO (03) Define extension function NetworkVideoHolder.asDatabaseModel(),
-// that returns an array of <DatabaseVideo>.
 
 /**
  * VideoHolder holds a list of Videos.
@@ -65,4 +64,18 @@ fun NetworkVideoContainer.asDomainModel(): List<Video> {
                 updated = it.updated,
                 thumbnail = it.thumbnail)
     }
+}
+
+
+// TODO (03) Define extension function NetworkVideoHolder.asDatabaseModel(),
+// that returns an array of <DatabaseVideo>.
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo (
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }.toTypedArray()
 }
